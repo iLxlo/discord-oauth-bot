cfb4e622490440dd31b50052d709e2c7be599e5c
iLxlo <98545753+iLxlo@users.noreply.github.com>
2023-09-20 15:28:12 +0300

    Update userSchema.js

--- Schema/userSchema.js
+++ Schema/userSchema.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 
 const authSchema = new mongoose.Schema({
 
-    id: { type: String, require: true },
-    username: { type: String, require: true },
-    discriminator: { type: String, require: true },
-    avatar: { type: String, require: true },
-    accessToken: { type: String, require: true },
-    refreshToken: { type: String, require: true },
-    ip: { type: String, require: true },
-    locale: { type: String, require: true },
-    refreshed: { type: Boolean, require: true },
-    expiresDate: { type: Number, require: true, default: null },
+  id: { type: String, require: true },
+  username: { type: String, require: true },
+  discriminator: { type: String, require: true },
+  avatar: { type: String, require: true },
+  accessToken: { type: String, require: true },
+  refreshToken: { type: String, require: true },
+  ip: { type: String, require: true },
+  locale: { type: String, require: true },
+  refreshed: { type: Boolean, require: true },
+  expiresDate: { type: Number, require: true, default: null },
 
 });
 
