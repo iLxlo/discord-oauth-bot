5c7af64f0edf3e72b22c9b9a67ab6e92eb31e59c
iLxlo <98545753+iLxlo@users.noreply.github.com>
2023-09-20 15:28:45 +0300

    Update client.js

--- Utils/client.js
+++ Utils/client.js
@@ -3,6 +3,9 @@ const client = new Discord.Client({ intents: [3276799]});
 client.slashcommands = new Discord.Collection();
 client.newCollection = new Discord.Collection();
 
-client.login(require('../Settings/config').token);
+client.login(require('../Settings/config').token).then(() => {
+  console.log(
+    ` Successfully logged in as: ${client.user.tag} ${client.user.id}`);
+});
 
 module.exports = client;
